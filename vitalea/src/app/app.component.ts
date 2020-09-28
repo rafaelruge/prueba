77b373dd787bdd51ad08f7de192d9556ee092dc2
rafaelruge <44501739+rafaelruge@users.noreply.github.com>
2020-09-28 12:07:59 -0500

    prueba tecnica front end

---
+++ vitalea/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'vitalea';
+
+  body1: string = 'SALUD PROACTIVA A TU ALCANCE';
+
+  body2: string = 'PASO A PASO';
+
+  body3: string = 'CHEQUEOS';
+
+
+}
---
+++ vitalea/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//bootstrap
+
+
+//componenetes
+import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SliderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
